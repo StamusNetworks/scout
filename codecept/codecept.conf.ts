1f493f7774f106bc16b531aef390dc5b5adc64f4
Mathias Kurz <mkurz@stamus-networks.com>
2025-11-20 11:56:07 +0100

    ci: run e2e in headless mode

--- codecept/codecept.conf.ts
+++ codecept/codecept.conf.ts
@@ -1,3 +1,11 @@
+import { setCommonPlugins, setHeadlessWhen } from '@codeceptjs/configure';
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
 export const config: CodeceptJS.MainConfig = {
   tests: './**/*.test.ts',
   output: './output',
