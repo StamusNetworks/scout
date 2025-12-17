621df8a060cdfde2d1182a1391451d872d7511e0
Mathias Kurz <mkurz@stamus-networks.com>
2025-12-17 09:55:22 +0100

    chore: remove tags from investigation in CE

--- src/features/hunt/investigation/components/investigation-details/investigation-params.tsx
+++ src/features/hunt/investigation/components/investigation-details/investigation-params.tsx
@@ -5,6 +5,7 @@ import { Card } from '@/common/design-system/atoms/ui/card';
 import { DateTime } from '@/common/design-system/entities/date-time';
 import { CheckboxValue } from '@/common/design-system/molecules/checkbox-value';
 import { LabelValue } from '@/common/design-system/molecules/label-value';
+import { useFeatureFlags } from '@/common/lib/use-feature-flags';
 import { cn } from '@/common/lib/utils';
 import { EventValue } from '@/features/hunt/filtering/query-filters/components/event-value/event-value';
 import { QueryFilterState } from '@/features/hunt/filtering/query-filters/model/query-filter';
@@ -16,7 +17,7 @@ import { valueVariants } from '../ongoing-investigation/ongoing-investigation.cu
 interface InvestigationParamsProps {
   startDate: number;
   endDate: number;
-  tags: TagFilters;
+  tags: TagFilters | null;
   qfilter: QueryFilterState[];
   comment?: string;
 }
@@ -26,103 +27,111 @@ export const InvestigationParams = ({
   tags,
   comment,
   qfilter,
-}: InvestigationParamsProps) => (
-  <Column className="gap-4">
-    <Row className="gap-8">
-      <LabelValue
-        label="Start date"
-        value={<DateTime date={startDate} />}
-      />
-      <LabelValue
-        label="End date"
-        value={<DateTime date={endDate} />}
-      />
-      <LabelValue
-        label="Events types"
-        value={
-          <Column className="mt-1 gap-1">
-            <CheckboxValue
-              checked={tags.alert}
-              label="Alerts"
-            />
-            <CheckboxValue
-              checked={tags.stamus}
-              label="Stamus"
-            />
-            <CheckboxValue
-              checked={tags.discovery}
-              label="Sightings"
-            />
-          </Column>
-        }
-      />
-      <LabelValue
-        label="Alert tags"
-        value={
-          <Column className="mt-1 gap-1">
-            <CheckboxValue
-              checked={tags.informational}
-              label="Informational"
-            />
-            <CheckboxValue
-              checked={tags.relevant}
-              label="Relevant"
-            />
-            <CheckboxValue
-              checked={tags.untagged}
-              label="Untagged"
-            />
-          </Column>
-        }
-      />
-      <LabelValue
-        label="Other"
-        value={
-          <Column className="mt-1 gap-1">
-            <CheckboxValue
-              checked={tags.novelty}
-              label="Outlier events"
-            />
-          </Column>
-        }
-      />
-      {comment && (
+}: InvestigationParamsProps) => {
+  const { enterprise } = useFeatureFlags();
+  return (
+    <Column className="gap-4">
+      <Row className="gap-8">
         <LabelValue
-          label="Comment"
-          value={comment}
+          label="Start date"
+          value={<DateTime date={startDate} />}
+        />
+        <LabelValue
+          label="End date"
+          value={<DateTime date={endDate} />}
+        />
+        {enterprise && tags && (
+          <>
+            <LabelValue
+              label="Events types"
+              value={
+                <Column className="mt-1 gap-1">
+                  <CheckboxValue
+                    checked={tags.alert}
+                    label="Alerts"
+                  />
+                  <CheckboxValue
+                    checked={tags.stamus}
+                    label="Stamus"
+                  />
+                  <CheckboxValue
+                    checked={tags.discovery}
+                    label="Sightings"
+                  />
+                </Column>
+              }
+            />
+            <LabelValue
+              label="Alert tags"
+              value={
+                <Column className="mt-1 gap-1">
+                  <CheckboxValue
+                    checked={tags.informational}
+                    label="Informational"
+                  />
+                  <CheckboxValue
+                    checked={tags.relevant}
+                    label="Relevant"
+                  />
+                  <CheckboxValue
+                    checked={tags.untagged}
+                    label="Untagged"
+                  />
+                </Column>
+              }
+            />
+            <LabelValue
+              label="Other"
+              value={
+                <Column className="mt-1 gap-1">
+                  <CheckboxValue
+                    checked={tags.novelty}
+                    label="Outlier events"
+                  />
+                </Column>
+              }
+            />
+          </>
+        )}
+
+        {comment && (
+          <LabelValue
+            label="Comment"
+            value={comment}
+          />
+        )}
+      </Row>
+      {qfilter.length > 0 && (
+        <LabelValue
+          label="Query filters"
+          value={
+            <Grid className="mt-1 grid-cols-4 gap-2">
+              {qfilter.map((filter, i) => {
+                return (
+                  <Card
+                    key={i}
+                    className={cn(
+                      'cursor-default rounded-md p-2 py-1',
+                      valueVariants({
+                        status: filter.is_suspended ? 'ignored' : 'undefined',
+                      }),
+                    )}
+                  >
+                    <label className="text-xs font-medium opacity-50">
+                      {getFilterLabel(filter.key)}
+                    </label>
+                    <EventValue
+                      query_key={filter.key}
+                      value={filter.value}
+                      className="whitespace-pre-wrap text-inherit"
+                    />
+                  </Card>
+                );
+              })}
+            </Grid>
+          }
         />
       )}
-    </Row>
-    {qfilter.length > 0 && (
-      <LabelValue
-        label="Query filters"
-        value={
-          <Grid className="mt-1 grid-cols-4 gap-2">
-            {qfilter.map((filter, i) => {
-              return (
-                <Card
-                  key={i}
-                  className={cn(
-                    'cursor-default rounded-md p-2 py-1',
-                    valueVariants({
-                      status: filter.is_suspended ? 'ignored' : 'undefined',
-                    }),
-                  )}
-                >
-                  <label className="text-xs font-medium opacity-50">
-                    {getFilterLabel(filter.key)}
-                  </label>
-                  <EventValue
-                    query_key={filter.key}
-                    value={filter.value}
-                    className="whitespace-pre-wrap text-inherit"
-                  />
-                </Card>
-              );
-            })}
-          </Grid>
-        }
-      />
-    )}
-  </Column>
-);
+    </Column>
+  );
+};
