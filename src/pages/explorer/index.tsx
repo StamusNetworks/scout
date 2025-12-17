c96b408827d536c522cfa1e754ca5a82cab0b31b
Mathias Kurz <mkurz@stamus-networks.com>
2025-12-17 11:25:39 +0100

    chore: remove unused imports

--- src/pages/explorer/index.tsx
+++ src/pages/explorer/index.tsx
@@ -1,12 +1,10 @@
 import { Binary } from 'lucide-react';
 import { keys, values } from 'ramda';
-import { Link } from 'react-router-dom';
 
 import { DefaultPage } from '@/common/design-system/atoms/default-page';
 import { Column } from '@/common/design-system/atoms/layout/column';
 import { Grid } from '@/common/design-system/atoms/layout/grid';
 import { Row } from '@/common/design-system/atoms/layout/row';
-import { Button } from '@/common/design-system/atoms/ui/button';
 import {
   Empty,
   EmptyContent,
@@ -17,7 +15,6 @@ import {
 import { Spin } from '@/common/design-system/atoms/ui/spin';
 import { OutletBreadcrumb } from '@/common/design-system/molecules/breadcrumbs';
 import { usePageTitle } from '@/common/lib/use-page-title';
-import { getConfig } from '@/config';
 import { useDashboard } from '@/features/hunt/dashboard/api/hooks/useDashboard';
 import { DashboardPanels } from '@/features/hunt/dashboard/components/dashboard-panels';
 import { EventsCounter } from '@/features/hunt/dashboard/components/events-counter';
@@ -26,13 +23,10 @@ import { ExpandCollapseRows } from '@/features/hunt/dashboard/components/expand-
 import { HideEmptyPanels } from '@/features/hunt/dashboard/components/hide-empty-tiles';
 import { OrderingSelector } from '@/features/hunt/dashboard/components/ordering-selector';
 import { PageSizeSelector } from '@/features/hunt/dashboard/components/page-size-selector';
-import { useGetSystemSettingsQuery } from '@/features/user/settings/settings.api';
 
 export const Explorer = () => {
   usePageTitle('Explorer');
   const { data: dashboardData, isLoading } = useDashboard();
-  const { data: systemSettings } = useGetSystemSettingsQuery();
-  const kibana_url = systemSettings?.kibana_url;
 
   return (
     <>
