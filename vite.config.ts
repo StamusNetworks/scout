d2f7c7cdfc9f3e5f86a633bbca546e80c8f3028a
Mathias Kurz <mkurz@stamus-networks.com>
2025-12-29 11:33:02 +0100

    chore: use env variable for proxy adress
    Tired of proxying to different VMs and switching branch resetting the sessionid.

--- vite.config.ts
+++ vite.config.ts
@@ -3,37 +3,40 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    host: '0.0.0.0',
-    proxy: {
-      '/rest': {
-        target: 'https://10.136.4.9/rest',
-        rewrite: (path) => path.replace(/^\/rest/, ''),
-        changeOrigin: true,
-        secure: false,
-      },
-      '/blog': {
-        target: 'https://10.136.4.9/blog',
-        rewrite: (path) => path.replace(/^\/blog/, ''),
-        changeOrigin: true,
-        secure: false,
-      },
-      '/api': {
-        target: 'https://10.136.4.9/api',
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        changeOrigin: true,
-        secure: false,
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
     },
-  },
-});
+    server: {
+      host: '0.0.0.0',
+      proxy: {
+        '/rest': {
+          target: `https://${process.env.VITE_PROXY}/rest`,
+          rewrite: (path) => path.replace(/^\/rest/, ''),
+          changeOrigin: true,
+          secure: false,
+        },
+        '/blog': {
+          target: `https://${process.env.VITE_PROXY}/blog`,
+          rewrite: (path) => path.replace(/^\/blog/, ''),
+          changeOrigin: true,
+          secure: false,
+        },
+        '/api': {
+          target: `https://${process.env.VITE_PROXY}/api`,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  });
+};
