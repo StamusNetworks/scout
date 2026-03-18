94c80c8b84b8f6c640db39a5c95e0f6ff98476d2
Mathias Kurz <mkurz@stamus-networks.com>
2026-03-18 17:23:36 +0100

    feat(threats): add policy-violations entities
    Add self-contained entity components for policy violations:
    - PolicyViolationsEntities: renders ImpactedEntitiesTable with familyClass="dopv"
    - PolicyViolationsGraph: renders EntitiesForceGraph with familyClass="dopv"

---
+++ src/features/threats/policy-violations/use-cases/entities/entities/policy-violations-entities.tsx
@@ -0,0 +1,5 @@
+import { ImpactedEntitiesTable } from '@/features/threats/common/molecules/impacted-entities-table/impacted-entities-table';
+
+export const PolicyViolationsEntities = () => {
+  return <ImpactedEntitiesTable familyClass="dopv" />;
+};
---
+++ src/features/threats/policy-violations/use-cases/graph/entities/policy-violations-graph.tsx
@@ -0,0 +1,5 @@
+import { EntitiesForceGraph } from '@/features/threats/common/molecules/entities-force-graph/entities-force-graph';
+
+export const PolicyViolationsGraph = () => {
+  return <EntitiesForceGraph familyClass="dopv" />;
+};
