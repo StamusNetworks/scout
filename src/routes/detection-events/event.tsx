8689c20b20b6b3f7343960ee3abf9305e726ed8b
Mathias Kurz <mkurz@stamus-networks.com>
2026-03-18 08:43:33 +0100

    refactor: slim /detection-events/event to thin orchestrator
    Replace EventByIdPage with EventDetail entity. Add Zod search schema
    for _id param. Delete unused pages/events/:eventId/index.tsx (no
    remaining imports).

--- src/routes/detection-events/event.tsx
+++ src/routes/detection-events/event.tsx
@@ -1,12 +1,31 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { z } from 'zod';
 
 import { PageBoundary } from '@/common/design-system/atoms/error-boundary';
-import { EventByIdPage } from '@/pages/events/:eventId';
+import { Page, PageContainer } from '@/common/design-system/atoms/page';
+import { EventDetail } from '@/features/events/common/event-detail/entities/event-detail';
+
+const searchSchema = z.object({
+  _id: z.string(),
+});
 
 export const Route = createFileRoute('/detection-events/event')({
+  validateSearch: searchSchema,
   component: () => (
     <PageBoundary key="event-by-id">
-      <EventByIdPage />
+      <EventDetailPage />
     </PageBoundary>
   ),
 });
+
+function EventDetailPage() {
+  const { _id } = Route.useSearch();
+
+  return (
+    <Page>
+      <PageContainer>
+        <EventDetail eventId={_id} />
+      </PageContainer>
+    </Page>
+  );
+}
