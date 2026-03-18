3206e0a7b58eaeed3f9802e74e860ef3c1354f32
Mathias Kurz <mkurz@stamus-networks.com>
2026-03-18 17:25:24 +0100

    chore: delete old policy-violations pages
    Remove src/pages/policy-violations/ entirely. All logic now lives in
    feature entities and route orchestrators.

--- src/pages/policy-violations/impacted-entities/index.tsx
+++
@@ -1,5 +0,0 @@
-import { ImpactedEntitiesTable } from '@/features/hunt/entities/components/impacted-entities-table/impacted-entities-table';
-
-export const PolicyViolationsImpactedEntities = () => {
-  return <ImpactedEntitiesTable familyClass="dopv" />;
-};
